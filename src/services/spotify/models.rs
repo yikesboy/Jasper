b362ff3f6be8ff7feab91235cdd5e1770b7a8a5e
JL <115737916+yikesboy@users.noreply.github.com>
2025-12-22 22:01:48 +0100

    feat: defined spotify api models

---
+++ src/services/spotify/models.rs
@@ -0,0 +1,28 @@
+use serde::Deserialize;
+#[derive(Deserialize, Debug)]
+pub struct TokenResponse {
+    pub access_token: String,
+    pub token_type: String,
+    pub expires_in: i32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PlaylistTracks {
+    pub items: Vec<PlaylistItem>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PlaylistItem {
+    pub track: FullTrack,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct FullTrack {
+    pub name: String,
+    pub artists: Vec<Artist>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Artist {
+    pub name: String,
+}
