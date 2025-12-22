325df1169c3c247db1b098ce19e12fa157171d24
JL <115737916+yikesboy@users.noreply.github.com>
2025-12-22 22:01:36 +0100

    feat: defined SpotifyAPI errors

---
+++ src/services/spotify/error.rs
@@ -0,0 +1,25 @@
+use thiserror::Error;
+use url::Url;
+#[derive(Error, Debug)]
+pub enum SpotifyAPIError {
+    #[error("Not a spotify link: {0}")]
+    InvalidLink(Url),
+
+    #[error("Malformed url: {0}")]
+    MalformedUrl(Url),
+
+    #[error("Not a playlist link: {0}")]
+    NotPlaylistLink(Url),
+
+    #[error("Unexpected Link Segment: {0}")]
+    UnexpectedSegment(String),
+
+    #[error("Playlist Id Missing")]
+    PlaylistIdMissing,
+
+    #[error("Failed to authenticate: {0}")]
+    FailedToAuthenticate(String),
+
+    #[error("Failed to retrieve playlist tracks")]
+    FailedToRetrievePlaylistTracks,
+}
