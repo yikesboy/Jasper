59985902d401a0fdee6ef7d7a10962efc784e2a4
JL <115737916+yikesboy@users.noreply.github.com>
2026-04-09 19:42:09 +0200

    feat: ping pong now works

--- src/commands/general/ping.rs
+++ src/commands/general/ping.rs
@@ -2,6 +2,6 @@ use crate::{Context, Error};
 
 #[poise::command(slash_command, prefix_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.defer_ephemeral().await?;
+    ctx.say("pong!").await?;
     Ok(())
 }
